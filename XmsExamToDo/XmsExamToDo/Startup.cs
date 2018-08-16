20143a445ec8afc4911c6805e79dc0849f472bc0
rubabramzan96 <rubabramzan96@gmail.com>
2018-08-16 17:54:08 +0100

    project set up

---
+++ XmsExamToDo/XmsExamToDo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(XmsExamToDo.Startup))]
+namespace XmsExamToDo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
