2e92b91fd7bc0831ee472a2853d9abb0de180ce7
s00165427 <s00165427@mail.itsligo.ie>
2018-08-17 11:58:01 +0100

    Q1a classes and library context

--- XmsExamToDo/XmsExamToDo/Models/IdentityModels.cs
+++ XmsExamToDo/XmsExamToDo/Models/IdentityModels.cs
@@ -21,8 +21,7 @@ namespace XmsExamToDo.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false) 
-
+            : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
 
---
+++ XmsExamToDo/XmsExamToDo/Models/LibraryContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace XmsExamToDo.Models
+{
+    public class LibraryContext: DbContext
+    {
+        public LibraryContext() : base("DefaultConnection")
+        {
+            Configuration.LazyLoadingEnabled = false;
+        }
+        public DbSet<Member> Members { get; set; }
+        public DbSet<Loan> Loans { get; set; }
+        public DbSet<Book> books { get; set; }
+    }
+}
